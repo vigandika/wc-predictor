7df03a821ec827266e93aa9583f513a6f0132eb4
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2022-11-23 00:34:18 +0100

    Implement scoring system

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,16 +1,125 @@
 <template>
-  <div class="">
-    <p> Lol </p>
-  </div>
+  <v-container>
+    <template>
+      <v-expansion-panels v-model="history">
+        <v-expansion-panel v-for="(key, value) in Object.entries(history)" :key="value">
+          <v-expansion-panel-header> {{key}} {{value}} </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sits amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </template>
+  </v-container>
 </template>
 
-
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
+import { Match } from "@/models/Match";
 
-@Component({
-})
+type MatchHistory = {
+  homeTeam: string;
+  awayTeam: string;
+  homeScore: number;
+  awayScore: number;
+  predictions: Record<string, { predictedHomeScore: number; predictedAwayScore: number }> | any;
+};
+
+@Component({})
 export default class History extends Vue {
+  private history: Record<string, MatchHistory> = {};
+  private predictions: Array<any> = [];
 
+  mounted() {
+    let data = [
+      {
+        username: "myrteza",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 3,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "ilir",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 5,
+        predictedAwayScore: 1,
+      },
+      {
+        username: "hana",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 5,
+        predictedAwayScore: 1,
+      },
+      {
+        username: "myrteza",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 2,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "ilir",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 0,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "hana",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 1,
+        predictedAwayScore: 3,
+      },
+    ];
+    this.predictions = data;
+    this.populateHistory();
+  }
+
+  private populateHistory() {
+    this.predictions.forEach((prediction) => {
+      if (prediction.match in this.history) {
+        let homePred = prediction.predictedHomeScore;
+        let awayPred = prediction.predictedAwayScore;
+        this.history[prediction.match].predictions[prediction.username] = { homePred, awayPred };
+      } else {
+        this.history[prediction.match] = {
+          homeTeam: prediction.homeTeam,
+          awayTeam: prediction.awayTeam,
+          homeScore: prediction.homeScore,
+          awayScore: prediction.awayScore,
+          predictions: {},
+        };
+
+        let homePred = prediction.predictedHomeScore;
+        let awayPred = prediction.predictedAwayScore;
+        this.history[prediction.match].predictions = {
+          [prediction.username]: { homePred, awayPred },
+        };
+      }
+    });
+  }
 }
 </script>
--- src/components/Rankings.vue
+++ src/components/Rankings.vue
@@ -1,7 +1,7 @@
 <template>
-<v-container>
-    <p> Lol </p>
-</v-container>
+  <v-container>
+    <v-data-table dense :headers="headers" :items="rankings" hide-default-footer class="elevation-1"></v-data-table>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -14,14 +14,129 @@ import moment from "moment";
 export default class Rankings extends Vue {
   private axios = axios.create({});
   private matches: Array<Match> = [];
+  private rankings = [];
+  private predictions: Array<any> = [];
   private date = moment();
 
-  mounted() {
+  get headers() {
+    return [
+      { text: "User", value: "username" },
+      { text: "Predictions", value: "matchesPredicted" },
+      { text: "Points", value: "points" },
+    ];
   }
 
+  mounted() {
+    let data = [
+      {
+        username: "myrteza",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 3,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "ilir",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 5,
+        predictedAwayScore: 1,
+      },
+      {
+        username: "hana",
+        match: "englandiran",
+        homeTeam: "england",
+        awayTeam: "iran",
+        homeScore: 6,
+        awayScore: 2,
+        predictedHomeScore: 6,
+        predictedAwayScore: 2,
+      },
+      {
+        username: "myrteza",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 2,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "ilir",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 0,
+        predictedAwayScore: 0,
+      },
+      {
+        username: "hana",
+        match: "FranceAustralia",
+        homeTeam: "France",
+        awayTeam: "Australia",
+        homeScore: 4,
+        awayScore: 1,
+        predictedHomeScore: 1,
+        predictedAwayScore: 3,
+      },
+    ];
+    this.predictions = data;
+    this.calculatePoints();
+  }
+
+  private calculatePoints() {
+    let map: any = {};
+    this.predictions.forEach((prediction) => {
+      console.log(prediction);
+      let score = 0;
+      let predictedHeuristics = prediction.predictedHomeScore - prediction.predictedAwayScore;
+      let realHeuristics = prediction.homeScore - prediction.awayScore;
+
+      console.log(predictedHeuristics, realHeuristics)
+      if (predictedHeuristics * realHeuristics > 0 || (predictedHeuristics === 0 && realHeuristics === 0)) {
+        // if both heuristics have the same sign (multiply to zero), winner is correctly predicted
+        // if both are zero, draw is correctly predicted
+        score += 3;
+        console.log("giving +3 to " + prediction.username + " for match " + prediction.match);
+      }
+
+      if (Math.abs(predictedHeuristics) === Math.abs(realHeuristics)) {
+        // if difference between score is correctly predicted, extra +1
+        score += 1;
+      }
+
+      if (prediction.homeScore === prediction.predictedHomeScore && prediction.awayScore === prediction.predictedAwayScore) {
+        // exact score, +1
+        score += 1;
+      }
+
+      if (prediction.username in map) {
+        map[prediction.username].matchesPredicted += 1;
+        map[prediction.username].points += score;
+      } else {
+        map[prediction.username] = {
+          username: prediction.username,
+          matchesPredicted: 1,
+          points: score,
+        };
+      }
+    });
+
+    console.log(map);
+    // @ts-ignore
+    this.rankings = Object.values(map);
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
